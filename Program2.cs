9a9005ba9b31772d2afc9ec9bc2fe39845e016aa
Amorcito1 <149332725+Amorcito1@users.noreply.github.com>
2025-08-05 10:56:47 +0000

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace NewConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age:");
+            string input = Console.ReadLine();
+
+
+            if (int.TryParse(input, out int age))
+            {
+                
+                int ticketPrice;
+
+                if (age <= 12 || age >= 65)
+                {
+                    ticketPrice = 7; 
+                }
+                else
+                {
+                    ticketPrice = 10; 
+                }
+
+                Console.WriteLine($"The ticket price is: GHC{ticketPrice}");
+            }
+            else
+            {
+                Console.WriteLine;
+            }
+        }
+    }
+}
